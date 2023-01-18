edd7a8e8fc60992ca276998eb3da82f714a5b138
PrajaktaShewale <shewalep94@gmail.com>
2023-01-18 14:41:35 +0530

    [Prajakta]Add:Program to write Emp Wages using nodeJS

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -1,6 +1,6 @@
 //UC1
 const IS_ABSENT = 0
-let empCheck = Math.floor(Math.random() * 10)% 2;
+let empCheck = Math.floor(Math.random() * 10)% 2;// Returns a random integer from 0 to 9
 if (empCheck == IS_ABSENT){
     console.log("Emplyoee is Absent");
     return ;
