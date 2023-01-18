15329e0a1c3edb50535a66976135b9a02a0838a1
PrajaktaShewale <shewalep94@gmail.com>
2023-01-18 15:03:23 +0530

    [Prajakta]Add:Program to write Emp Wages and get work hrs from function using nodeJS

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -1,9 +1,23 @@
-//UC1
-const IS_ABSENT = 0
-let empCheck = Math.floor(Math.random() * 10)% 2;// Returns a random integer from 0 to 9
-if (empCheck == IS_ABSENT){
-    console.log("Emplyoee is Absent");
-    return ;
-}else{
-    console.log("Employee is Present");
+//UC3
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+let empHrs = 0;
+empCheck = Math.floor(Math.random()*10)%3;
+console.log(empCheck);
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;            
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+                default:
+                    return 0;
+    }
 }
+empHrs= getWorkingHours(empCheck);
+let empWage = empHrs*WAGE_PER_HOURS;
+console.log("Emp Wage : "+ empWage);
