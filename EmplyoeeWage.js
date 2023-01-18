ce281e8cc553bcb9e05b689a62fab64871a6d21a
PrajaktaShewale <shewalep94@gmail.com>
2023-01-18 15:34:39 +0530

    [Prajakta]Add:Program to write Emp Wages for a month and get work hrs from finctio using nodeJS

--- EmplyoeeWage.js
+++ EmplyoeeWage.js
@@ -1,13 +1,11 @@
-//UC3
-
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
+//UC4
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const WAGE_PER_HOURS = 20;
+const NUM_OF_WORKING_DAYS =20;
 const PART_TIME_HOURS =4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOURS = 20;
-let empHrs = 0;
-empCheck = Math.floor(Math.random()*10)%3;
-console.log(empCheck);
+let empHrs =0;
 function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -18,6 +16,10 @@ function getWorkingHours(empCheck){
                     return 0;
     }
 }
-empHrs= getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOURS;
-console.log("Emp Wage : "+ empWage);
+for(let day =0; day<NUM_OF_WORKING_DAYS; day++){
+    var empCheck = Math.floor(Math.random()*10)%3;
+    empHrs +=getWorkingHours(empCheck);
+}
+console.log(empCheck);
+let empWage = empHrs *WAGE_PER_HOURS;
+console.log("Total Hrs : "+ empHrs + "  EmpWage For MOnth : " + empWage);
